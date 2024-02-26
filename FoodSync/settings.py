b379e2e89dfca76a6c9c24bd49f8f213af0d9e21
Anthony <anthonynikhilreddy@gmail.com>
2024-02-26 18:59:17 -0500

    eb changes

--- FoodSync/settings.py
+++ FoodSync/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "http://inet-wednesday-spring2024-team-1-dev.us-east-2.elasticbeanstalk.com/"
+    ]
 
 
 # Application definition
