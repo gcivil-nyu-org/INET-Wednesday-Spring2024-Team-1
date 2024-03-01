9ef30f9df2947b7d30fbab76968d793e18c4b312
Abijith Pradeep <321niugnep@gmail.com>
2024-02-29 22:33:34 -0500

    removed venv

--- swe-project/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\321ni\Desktop\Year 2023- 2024\INET-Wednesday-Spring2024-Team-1\swe-project\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
