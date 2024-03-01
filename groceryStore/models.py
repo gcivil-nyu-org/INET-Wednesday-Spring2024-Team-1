77cf9b2589e553d492dac57cd6c87f8c8c05d5ce
Abijith Pradeep <321niugnep@gmail.com>
2024-02-29 22:11:05 -0500

    Added backend grocery store tables

---
+++ groceryStore/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Grocery(models.Model):
+    gid = models.AutoField(primary_key=True)
+    gname = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.gname
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    ]
+    oid = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    calories = models.IntegerField()
+    recipe = models.TextField()
+
+    def __str__(self):
+        return f"Order ID: {self.oid}, User: {self.user.username}, Status: {self.status}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    grocery = models.ForeignKey(Grocery, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"Order ID: {self.order.oid}, Grocery: {self.grocery.gname}, Quantity: {self.quantity}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=20)
+    addr = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user.username
---
+++ swe-project/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\321ni\Desktop\Year 2023- 2024\INET-Wednesday-Spring2024-Team-1\swe-project\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
