af4f5c1f1c41d638efcf8838028a3e747a993d4f
Abijith Pradeep <321niugnep@gmail.com>
2024-03-03 18:44:44 -0500

    register models with admin

--- groceryStore/admin.py
+++ groceryStore/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Grocery,Order,OrderItem,UserProfile
+
+admin.site.register(Grocery)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(UserProfile)
