f8a6f2829432ebd769d5f385b85d4610d758b2b5
Anthony <anthonynikhilreddy@gmail.com>
2024-03-12 19:33:52 -0400

    minor change preferences

--- homepage/views.py
+++ homepage/views.py
@@ -8,6 +8,7 @@ from utils.homepage_utils import homepage_recipes
 
 def homepage(request):
     preferences_updated = request.session.pop("preferences_updated", False)
+    check_user_preferences = request.session.get("check_user_preferences")
     # Check if preferences have been updated
     if preferences_updated:
         homepage_recipes(request)
@@ -16,4 +17,4 @@ def homepage(request):
         return render(request, "homepage/homepage.html", {"recipes": recipes_info})
     homepage_recipes(request)
     recipes_info = request.session.get("homepage_recipes_info")
-    return render(request, "homepage/homepage.html", {"recipes": recipes_info})
+    return render(request, "homepage/homepage.html", {"recipes": recipes_info, "check_user_preferences": check_user_preferences})
