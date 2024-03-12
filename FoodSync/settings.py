6488850b022a057d0bd9c32b62de1fb81689a415
Bhanu Prakash Poluparthi <bhanuprakash@bhanus-mbp.mynetworksettings.com>
2024-03-12 01:10:05 -0400

    Reformat

--- FoodSync/settings.py
+++ FoodSync/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
-    "rest_framework"
+    "rest_framework",
 ]
 
 SOCIALACCOUNT_LOGIN_ON_GET = True
--- groceryStore/urls.py
+++ groceryStore/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("get_order_data/", get_order_data, name="get_order_data"),
     path("get_orderitem_data/", get_orderitem_data, name="get_orderitem_data"),
     path("get_user_data/", get_user_data, name="get_user_data"),
-    path('api/groceries/<str:gname>/', get_grocery_details, name='get_grocery_details'),
+    path("api/groceries/<str:gname>/", get_grocery_details, name="get_grocery_details"),
 ]
