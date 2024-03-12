a8e2d09e1a853a2188961f7ce7b718cbc84f7da3
Anthony <anthonynikhilreddy@gmail.com>
2024-03-11 22:42:02 -0400

    preferences based api call

--- utils/homepage_utils.py
+++ utils/homepage_utils.py
@@ -1,18 +1,52 @@
 import requests
 import os
 
+from users import models
+
 
 def homepage_recipes(request):
 
-    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random"
+    if(request.user.email):
+        user = models.CustomUser.objects.get(email=request.user.email)
+        if user.preferences:
+            user_preferences = models.UserPreferences.objects.get(user=user)
+            diet = user_preferences.diet
+            cuisines = user_preferences.cuisines.values_list('name', flat=True)
+            allergies = user_preferences.allergies.values_list('name', flat=True)
+            cuisines = ",".join([cuisine.lower() for cuisine in cuisines])
+            allergies = ",".join([allergy.lower() for allergy in allergies])
+            url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
 
-    querystring = {"number": "16"}
+            querystring = {
+                "diet": diet,
+                "cuisine": cuisines,
+                "intolerances": allergies,
+                "number": "16",
+            }
 
-    headers = {
-        "X-RapidAPI-Key": os.environ.get("RAPID_API_KEY"),
-        "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    }
+            headers = {
+                "X-RapidAPI-Key": os.environ.get("RAPID_API_KEY"),
+                "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            }
 
-    response = requests.get(url, headers=headers, params=querystring)
+            response = requests.get(url, headers=headers, params=querystring)
 
-    request.session["homepage_recipes_info"] = response.json()
+            results = response.json()["results"]
+
+            recipes = {"recipes": results}
+
+            request.session["homepage_recipes_info"] = recipes
+
+        else:
+            url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random"
+
+            querystring = {"number": "16"}
+
+            headers = {
+                "X-RapidAPI-Key": os.environ.get("RAPID_API_KEY"),
+                "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            }
+
+            response = requests.get(url, headers=headers, params=querystring)
+
+            request.session["homepage_recipes_info"] = response.json()
