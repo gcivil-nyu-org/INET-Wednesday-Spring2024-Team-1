3a7b7601b22d0cce185565fbd8f8bbfa5580056b
Anthony <anthonynikhilreddy@gmail.com>
2024-03-12 12:03:37 -0400

    rds

--- FoodSync/settings.py
+++ FoodSync/settings.py
@@ -104,16 +104,24 @@ WSGI_APPLICATION = "FoodSync.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["team1_db_name"],
-        "USER": os.environ["team1_db_user"],
-        "PASSWORD": os.environ["team1_db_password"],
-        "HOST": os.environ["team1_db_host"],
-        "PORT": os.environ["team1_db_port"],
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgesql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": str(os.path.join(BASE_DIR, "db.sqlite3")),
+        }
+    }
 
 
 # Password validation
