cb1b65a0022d0c14784d7b001f6bdad3e6a4d5cf
Anthony <anthonynikhilreddy@gmail.com>
2024-03-11 20:01:15 -0400

    signals reformat

--- users/signals.py
+++ users/signals.py
@@ -11,17 +11,10 @@ def update_custom_user(sender, user, request, **kwargs):
     print("update_custom_user", user.email)
     # Retrieve the user data from the auth_user table
     auth_user = User.objects.get(email=user.email)
-
-    # print(auth_user.email)
-
-    # Check if the corresponding CustomUser already exists
     try:
         custom_user = CustomUser.objects.get(email=auth_user.email)
     except CustomUser.DoesNotExist:
-        # If CustomUser does not exist, create a new one
         custom_user = CustomUser(email=auth_user.email)
-
-        # Update CustomUser fields with auth_user data
         custom_user.username = auth_user.username
         custom_user.preferences = "False"
         custom_user.save()
