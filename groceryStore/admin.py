6ce3baa0cb352c42de50099fd873a34eda4994de
Abijith Pradeep <321niugnep@gmail.com>
2024-03-12 15:53:41 -0400

    modified admin

--- groceryStore/admin.py
+++ groceryStore/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Grocery, Order, OrderItem, UserProfile
 
-# Register your models here.
-
-from .models import Grocery,Order,OrderItem,UserProfile
 
 admin.site.register(Grocery)
 admin.site.register(Order)
