868181a4f8a6b963c0081142bfe2160136c710ea
Bhanu Prakash Poluparthi <bhanuprakash@bhanus-mbp.mynetworksettings.com>
2024-03-12 00:47:22 -0400

    Add Grocery Store API Functionality

--- groceryStore/urls.py
+++ groceryStore/urls.py
@@ -1,4 +1,5 @@
 from .views import get_grocery_data, get_order_data, get_orderitem_data, get_user_data
+from .api_views import get_grocery_details
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     path("get_order_data/", get_order_data, name="get_order_data"),
     path("get_orderitem_data/", get_orderitem_data, name="get_orderitem_data"),
     path("get_user_data/", get_user_data, name="get_user_data"),
+    path("api/groceries/<str:gname>/", get_grocery_details, name="get_grocery_details"),
 ]
