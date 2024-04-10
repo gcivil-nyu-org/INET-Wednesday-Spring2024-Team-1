52912c28b7a24ba52ce67a764412d1af368378fc
Anthony <anthonynikhilreddy@gmail.com>
2024-04-10 13:23:01 -0400

    debug false

--- FoodSync/settings.py
+++ FoodSync/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "secret_key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "http://foodsync-dev-env.us-east-2.elasticbeanstalk.com/",
